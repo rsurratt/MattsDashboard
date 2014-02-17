c72629bed7e7c017c4502c3c0a4d4561255a728c
Rob Surratt <rob@surrattfamily.com>
2014-02-17 11:16:58 -0500

    Exception handling

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -82,38 +82,44 @@ class DashboardController < ApplicationController
       data = { :relay => relay }
       goals = relay.goals
 
-      uri = URI.parse(relay.url)
-      req = Net::HTTP.new(uri.host, uri.port)
-      req.read_timeout = 60
-      res = req.start() do |http|
-        http.get(uri.request_uri)
+      logger.info("Fetching #{relay.url}")
+
+      begin
+        uri = URI.parse(relay.url)
+        req = Net::HTTP.new(uri.host, uri.port)
+        req.read_timeout = 60
+        res = req.start() do |http|
+          http.get(uri.request_uri)
+        end
+
+        body = res.body
+        if body.nil?
+          return data
+        end
+
+        body.scan(/<p id="tr-greeting-eventInfo-date">([^<]*)/) do |match|
+          data[:date] = parseDate(match[0])
+        end
+
+        section = body.scan(/<div id="tr-greeting-eventStats">\n(.*)\n/)
+
+        section[0][0].scan(/.*>([0-9]+) teams.*>([0-9]+) participants.*>(\$[0123456789,.]+)/) do |match|
+          data[:teams] = StatusValue.new(:teams, match[0], goals[:teams])
+          data[:participants] = StatusValue.new(:participants, match[1], goals[:participants])
+
+          raised = match[2]
+          raised += '.00' if !raised.include?('.')
+          data[:dollarsraised] = StatusValue.new(:dollarsraised, raised, goals[:dollarsraised])
+        end
+
+        #section[0][0].scan(/<strong>([0123456789.$,]+)<\/strong>&nbsp;([^.]+)/) { |match|
+        #  key = match[1].downcase.delete(" ").to_sym
+        #  key = :dollarsraised if key == :raised
+        #  data[key] = StatusValue.new(key, match[0], goals[key])
+        #}
+      rescue => ex
+        logging.error("Exception fetching #{relay.url}: #{ex.message}")
       end
-
-      body = res.body
-      if body.nil?
-        return data
-      end
-
-      body.scan(/<p id="tr-greeting-eventInfo-date">([^<]*)/) do |match|
-        data[:date] = parseDate(match[0])
-      end
-
-      section = body.scan(/<div id="tr-greeting-eventStats">\n(.*)\n/)
-
-      section[0][0].scan(/.*>([0-9]+) teams.*>([0-9]+) participants.*>(\$[0123456789,.]+)/) do |match|
-        data[:teams] = StatusValue.new(:teams, match[0], goals[:teams])
-        data[:participants] = StatusValue.new(:participants, match[1], goals[:participants])
-
-        raised = match[2]
-        raised += '.00' if !raised.include?('.')
-        data[:dollarsraised] = StatusValue.new(:dollarsraised, raised, goals[:dollarsraised])
-      end
-
-      #section[0][0].scan(/<strong>([0123456789.$,]+)<\/strong>&nbsp;([^.]+)/) { |match|
-      #  key = match[1].downcase.delete(" ").to_sym
-      #  key = :dollarsraised if key == :raised
-      #  data[key] = StatusValue.new(key, match[0], goals[key])
-      #}
 
       data
     end
